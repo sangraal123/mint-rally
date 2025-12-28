e71e4324f32ee628b87153d4237d09bb07bb383b
sangraal123 <googolheroicquests@gmail.com>
2025-12-28 11:50:31 +0900

    Pinataの読み込み方法を変更

--- frontend/utils/ipfs2http.ts
+++ frontend/utils/ipfs2http.ts
@@ -1,6 +1,6 @@
 export const ipfs2http = (ipfs: string) => {
   const rootCid = ipfs.split("ipfs://")[1]?.split("/")[0];
-  const domain = process.env.NEXT_PUBLIC_PINATA_GATEWAY || `${rootCid}.ipfs.w3s.link`
+  const domain = "gateway.pinata.cloud";
   const fileName = ipfs.split("ipfs://")[1]?.split("/")[1];
   return `https://${domain}/ipfs/${rootCid}/${fileName}`;
 };
